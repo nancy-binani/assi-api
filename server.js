f3a620bb8a12be61c16f0310a1fd7349aae9759a
nancy-binani <71276756+nancy-binani@users.noreply.github.com>
2021-07-17 20:37:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let express = require("express");
 let data = require("./data.json");
 let server = express();
 
-server.get("/assi",function(req,res){
+server.get("/test",function(req,res){
     res.json(data);
 })
 
